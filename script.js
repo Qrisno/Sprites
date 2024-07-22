0300389bc1ab7b8e15138af71b0c0a78a4aae6d0
nino kristesiashvili <kristesiashvilinino@gmail.com>
2024-07-22 13:26:59 +0400

    refactor: optimize animation frame staggering and add animations

--- script.js
+++ script.js
@@ -9,15 +9,31 @@ playerImage.src = './assets/shadow_dog.png';
 let x = 0;
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 2;
+const spriteAnimations = [];
+const animationsState = [
+    {name: 'idle', frames: 7},
+    {name: 'jump', frames: 7},
+];
+animationsState.forEach((state, index) => {
+    let frames = {
+        loc: [],
+    };
+    for(let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth;
+        let positionY =  index *spriteHeight;
+        let frame = {x: positionX, y: positionY};
+        frames.loc.push(frame);
+    }
+    spriteAnimations[state.name] = frames;
+});
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    let position = Math.floor(gameFrame/staggerFrames) %6;
-
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['idle'].loc.length;
+    let frameX = spriteWidth*position;
+    let frameY = spriteAnimations['idle'].loc[position].y;
     frameX = position*spriteWidth;
     ctx.drawImage(playerImage, frameX, frameY*spriteHeight, spriteWidth,spriteHeight, 0,0, spriteWidth, spriteHeight);
 
